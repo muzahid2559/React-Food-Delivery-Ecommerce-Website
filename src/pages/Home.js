bffc96b66821b51fe89393845c2c56d8423ff404
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-09-18 20:53:32 +0600

    Feature part

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,30 @@ import { Link } from "react-router-dom";
 
 import Category from "../components/UI/category/Category";
 
+import "../styles/home.css";
+
+import featureImg01 from "../assets/images/service-01.png";
+import featureImg02 from "../assets/images/service-02.png";
+import featureImg03 from "../assets/images/service-03.png";
+
+const featureData = [
+    {
+      title: "Quick Delivery",
+      imgUrl: featureImg01,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    },
+  
+    {
+      title: "Super Dine In",
+      imgUrl: featureImg02,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    },
+    {
+      title: "Easy Pick Up",
+      imgUrl: featureImg03,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    },
+  ];
 
 
 const Home = () => {
@@ -46,14 +70,14 @@ const Home = () => {
                   <p className=" d-flex align-items-center gap-2 ">
                     <span className="shipping__icon">
                       <i class="ri-car-line"></i>
-                    </span>{" "}
+                    </span>
                     No shipping charge
                   </p>
 
                   <p className=" d-flex align-items-center gap-2 ">
                     <span className="shipping__icon">
                       <i class="ri-shield-check-line"></i>
-                    </span>{" "}
+                    </span>
                     100% secure checkout
                   </p>
                 </div>
@@ -75,6 +99,42 @@ const Home = () => {
       </section>
 
 
+        {/* feature part */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h5 className="feature__subtitle mb-4">What we serve</h5>
+              <h2 className="feature__title">Just sit back at your place</h2>
+              <h2 className="feature__title">
+                we will <span>take care</span>
+              </h2>
+              <p className="mb-1 mt-4 feature__text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+                officiis?
+              </p>
+              <p className="feature__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, eius.
+              </p>
+            </Col>
+
+            {featureData.map((item, index) => (
+              <Col lg="4" md="6" sm="6" key={index} className="mt-5">
+                <div className="feature__item text-center px-5 py-3">
+                  <img
+                    src={item.imgUrl}
+                    alt="feature-img"
+                    className="w-25 mb-3"
+                  />
+                  <h5 className=" fw-bold mb-3">{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
 
 
 
