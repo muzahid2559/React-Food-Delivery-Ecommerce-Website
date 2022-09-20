8ae48cea7f0fece238eeaae4cd3d41158c4746ae
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-09-20 20:29:06 +0600

    search icon and selected menu and product-card added in AllFoods

--- src/pages/AllFoods.js
+++ src/pages/AllFoods.js
@@ -2,11 +2,53 @@ import React, { useState } from "react";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/common-section/CommonSection";
 
+import { Container, Row, Col } from "reactstrap";
+import products from "../assets/fake-data/products";
+import ProductCard from "../components/UI/product-card/ProductCard";
+
+import "../styles/all-foods.css";
+
 const AllFoods = () => {
   return (
     <Helmet title="All-Foods">
       <CommonSection title="All Foods" />
-      
+
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6" xs="12">
+              <div className="search__widget d-flex align-items-center justify-content-between ">
+                <input
+                  type="text"
+                  placeholder="I'm looking for...."
+                />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="6" xs="12" className="mb-5">
+              <div className="sorting__widget text-end">
+                <select className="w-50">
+                  <option>Default</option>
+                  <option value="ascending">Alphabetically, A-Z</option>
+                  <option value="descending">Alphabetically, Z-A</option>
+                  <option value="high-price">High Price</option>
+                  <option value="low-price">Low Price</option>
+                </select>
+              </div>
+            </Col>
+
+            {products.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
+                <ProductCard item={item} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
     </Helmet>
   );
 };
